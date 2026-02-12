ebb8090eb671ce0eaa0702a6cda74f2f4ceb58a5
iskana-fardan <devfard@gmail.com>
2026-02-12 11:50:44 +0200

    feat(frontend): create topbar component

--- frontend/src/admin/components/Topbar.tsx
+++ frontend/src/admin/components/Topbar.tsx
@@ -1,24 +1,119 @@
-import { AppBar, Box, Toolbar, Typography } from "@mui/material"
+import * as React from 'react';
+import { AppBar ,Toolbar, useTheme } from "@mui/material"
+import Box from '@mui/material/Box';
+import Avatar from '@mui/material/Avatar';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Tooltip from '@mui/material/Tooltip';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import Logout from '@mui/icons-material/Logout';
+
+
+
+const drawerWidth = 240;
 
 export default function Topbar() {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const theme = useTheme();
   return (
     <AppBar
-      position="static"
-      color="transparent"
-      elevation={0}
-      sx={{ borderBottom: "1px solid", borderColor: "divider" }}
+      position="fixed"
+        sx={{
+          width: `calc(100% - ${drawerWidth}px)`,
+          ml: `${drawerWidth}px`,
+          backgroundColor: theme.palette.background.default
+        }}
     >
       <Toolbar>
-        <Typography variant="h6">
-          Admin Dashboard
-        </Typography>
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>
+            Dashboard
+          </Typography>
 
-        <Box sx={{ flexGrow: 1 }} />
 
-        <Typography variant="body2">
-          Admin
-        </Typography>
+          <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
+            <Tooltip title="Account settings">
+                <IconButton
+                    onClick={handleClick}
+                    size="small"
+                    sx={{ ml: 2 }}
+                    aria-controls={open ? 'account-menu' : undefined}
+                    aria-haspopup="true"
+                    aria-expanded={open ? 'true' : undefined}
+                >
+                  <Avatar sx={{ width: 32, height: 32 }}>A</Avatar>
+                </IconButton>
+            </Tooltip>
+          </Box>
+          <Menu
+              anchorEl={anchorEl}
+              id="account-menu"
+              open={open}
+              onClose={handleClose}
+              onClick={handleClose}
+              slotProps={{
+                paper: {
+                  elevation: 0,
+                  sx: {
+                    overflow: 'visible',
+                    filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                    mt: 1.5,
+                    '& .MuiAvatar-root': {
+                      width: 32,
+                      height: 32,
+                      ml: -0.5,
+                      mr: 1,
+                    },
+                    '&::before': {
+                      content: '""',
+                      display: 'block',
+                      position: 'absolute',
+                      top: 0,
+                      right: 14,
+                      width: 10,
+                      height: 10,
+                      bgcolor: 'background.paper',
+                      transform: 'translateY(-50%) rotate(45deg)',
+                      zIndex: 0,
+                    },
+                  },
+                },
+              }}
+              transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+              anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+          >
+          <MenuItem onClick={handleClose}>
+            <ListItemIcon>
+              <AccountCircleIcon fontSize='small'/>
+            </ListItemIcon>
+            Profile
+          </MenuItem>
+
+          <Divider />
+        
+          <MenuItem onClick={handleClose}>
+            <ListItemIcon>
+              <Logout fontSize="small" />
+            </ListItemIcon>
+            Logout
+          </MenuItem>
+        </Menu>
       </Toolbar>
     </AppBar>
   )
 }
+
+
+
